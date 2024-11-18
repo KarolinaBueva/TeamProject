da28d1cb4267134b857c15f72c0c9bb05a59c687
karolina <bueva.karolina@gmail.com>
2024-11-18 11:19:10 +0300

    forth

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace TeamProject
+﻿using System.Diagnostics;
+using System.Drawing;
+
+namespace TeamProject
 {
     enum Frequency { Weekly, Monthly, Yearly }
     internal class Magazine
@@ -94,7 +97,7 @@
 
         public virtual string ToShortString()
         {
-            return $"Название-{name}, периодичность выхода журнала-{frequency}, дата выхода-{date}, номер тиража-{TirazhNum}, значение среднего рейтинга статей-{articles.Average()}";
+            return $"Название-{name}, периодичность выхода журнала-{frequency}, дата выхода-{date}, номер тиража-{TirazhNum}, значение среднего рейтинга статей-{AverageRating}";
         }
     }
     public class Person
@@ -110,7 +113,7 @@
             BirthDate = birthDate;
         }
 
-        public Person() : this("Имя ", "Фамилия ", DateTime.Now)
+        public Person() : this("Имя ", "Фамилия ", new DateTime(2001,1,1))
         {
         }
 
@@ -174,5 +177,88 @@
         {
             return $"Автор: {Author.ToShortString()}, Название: {Title}, Рейтинг: {Rating}";
         }
+        static void Main()
+        {  
+            Person p1 = new Person("ivan", "ivanov", new DateTime(1990, 1, 1)); 
+            Person p2 = new Person();
+
+            Console.WriteLine(p1.ToString()); Console.WriteLine(p1.ToShortString());
+            Console.WriteLine(p2.ToString()); Console.WriteLine(p2.ToShortString());
+            try
+            {
+                p1.BirthYear = 1985; 
+                Console.WriteLine($"новый год рождения: {p1.BirthYear}");
+                Console.WriteLine(p1.ToString());
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+
+
+            const int n = 1000;
+
+            Person[] arr1 = new Person[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr1[i] = new Person("ivan", "ivanov", new DateTime(2000,10,10));
+            }
+
+
+            Person[,] arr2 = new Person[n, n]; 
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arr2[i, j] = new Person("petr", "petrov", new DateTime(2005,12,5));
+                }
+            }
+
+
+            Person[][] arr3 = new Person[n][]; 
+            for (int i = 0; i < n; i++)
+            {
+                arr3[i] = new Person[n];
+                for (int j = 0; j < n; j++)
+                {
+                    arr3[i][j] = new Person("vasya", "vasiliev", new DateTime(2003,3,3));
+                }
+            }
+
+
+            // Измерение времени доступа к элементам одномерного массива
+            int start = Environment.TickCount;
+            for (int i = 0; i < n; i++)
+            {
+                string name = arr1[i].FirstName;
+            }
+            int duration = Environment.TickCount - start; Console.WriteLine($"Одномерный массив: {duration} мс");
+
+
+            // Измерение времени доступа к элементам двумерного прямоугольного массива
+            start = Environment.TickCount;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    string name = arr2[i, j].FirstName;
+                }
+            }
+            duration = Environment.TickCount - start;
+            Console.WriteLine($"Двумерный прямоугольный массив: {duration} мс");
+
+
+            // Измерение времени доступа к элементам двумерного ступенчатого массива
+            start = Environment.TickCount;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    string name = arr3[i][j].FirstName;
+                }
+            }
+            duration = Environment.TickCount - start;
+            Console.WriteLine($"Двумерный ступенчатый массив: {duration} мс");
+    }
     }
 }
