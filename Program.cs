8cb6690e126c400a096152f6c23e1513c944d823
karolina <bueva.karolina@gmail.com>
2024-11-23 08:02:38 +0300

    end

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Drawing;
+using System.Text;
 
 namespace TeamProject
 {
@@ -51,10 +52,7 @@ namespace TeamProject
         public DateTime Date
         {
             get => date;
-            set
-            {
-                date = value;
-            }
+            set =>  date = value;
         }
 
         public int Num
@@ -82,17 +80,37 @@ namespace TeamProject
 
         public bool this[Frequency frequency]
         {
-            get => frequency == frequency;
+            get => this.frequency == frequency;
         }
 
-        public void AddArticles(params Article[] articles)
+        public Article[] Add(params Article[] art)
         {
-            articles = articles.Concat(articles).ToArray();
+            Article[] arr = new Article[articles.Length + art.Length];
+            for (int i = 0; i < articles.Length; i++)
+            {
+                arr[i] = articles[i];
+            }
+            for (int i = articles.Length; i < articles.Length+art.Length; i++)
+            {
+                arr[i] = art[i];
+            }
+            articles = arr;
+            return articles;
+        }
+
+        private string Out()
+        {
+            StringBuilder sb= new StringBuilder();  
+            foreach (Article i in articles)
+            {
+                sb.AppendLine(i.ToString());
+            }
+            return sb.ToString();
         }
 
         public override string ToString()
         {
-            return $"Название-{name}, периодичность выхода журнала-{frequency}, дата выхода-{date}, номер тиража-{TirazhNum}, список статей-{articles}";
+            return $"Название-{name}, периодичность выхода журнала-{frequency}, дата выхода-{date}, номер тиража-{TirazhNum}, список статей-{Out()}";//
         }
 
         public virtual string ToShortString()
@@ -177,28 +195,49 @@ namespace TeamProject
         {
             return $"Автор: {Author.ToShortString()}, Название: {Title}, Рейтинг: {Rating}";
         }
+    }
+    class Program { 
         static void Main()
         {  
             Person p1 = new Person("ivan", "ivanov", new DateTime(1990, 1, 1)); 
             Person p2 = new Person();
 
-            Console.WriteLine(p1.ToString()); Console.WriteLine(p1.ToShortString());
-            Console.WriteLine(p2.ToString()); Console.WriteLine(p2.ToShortString());
+            Article article1 = new Article(p1, "Статья 1", 4.5);
+            Article article2 = new Article(p2, "Статья 2", 4.8);
+            Article article3 = new Article(p1, "Статья 3", 4.2);
+
+            Magazine magazine1 = new Magazine("magaz", Frequency.Weekly, DateTime.Now, 5000);
+            Magazine magazine2 = new Magazine();
+
+            magazine1.Add(article1, article2);
+
+            Console.WriteLine(magazine1);
+            Console.WriteLine();
+            Console.WriteLine(magazine2.ToShortString());
+
+
+            magazine2.Articles = new[] { article3 };
+
+
+            Console.WriteLine($"Средний рейтинг статей в magazine1: {magazine1.AverageRating}");
+            Console.WriteLine($"Средний рейтинг статей в magazine2: {magazine2.AverageRating}");
+
+            Console.WriteLine($"информация об авторе 1: {p1}");
+            Console.WriteLine($"информация об авторе 2: {p2.ToShortString()}");
+
             try
             {
-                p1.BirthYear = 1985; 
-                Console.WriteLine($"новый год рождения: {p1.BirthYear}");
-                Console.WriteLine(p1.ToString());
+                p1.BirthYear = 1980;
+                Console.WriteLine($"Измененный год рождения автора 1: {p1.BirthDate.Year}");
             }
-            catch (ArgumentException ex)
+            catch (ArgumentException e)
             {
-                Console.WriteLine($"Ошибка: {ex.Message}");
+                Console.WriteLine(e.Message);
             }
 
+        const int n = 5000;
 
-            const int n = 1000;
-
-            Person[] arr1 = new Person[n];
+            Person[] arr1 = new Person[n*n];
             for (int i = 0; i < n; i++)
             {
                 arr1[i] = new Person("ivan", "ivanov", new DateTime(2000,10,10));
@@ -232,7 +271,8 @@ namespace TeamProject
             {
                 string name = arr1[i].FirstName;
             }
-            int duration = Environment.TickCount - start; Console.WriteLine($"Одномерный массив: {duration} мс");
+            int duration = Environment.TickCount - start; 
+            Console.WriteLine($"Одномерный массив: {duration} мс");
 
 
             // Измерение времени доступа к элементам двумерного прямоугольного массива
